60078f526192e07a204c8cb710ab60b38cbe0b02
Araraura <25017422+Araraura@users.noreply.github.com>
2022-08-17 21:19:31 +0300

    Added support for FuryAlwaysOn mod

--- FragileStrengthForNailArts.cs
+++ FragileStrengthForNailArts.cs
@@ -1,4 +1,5 @@
-﻿using Modding;
+﻿using System;
+using Modding;
 using System.Reflection;
 using HutongGames.PlayMaker;
 
@@ -21,10 +22,21 @@ namespace FragileStrengthForNailArts
 		{
 			int  damageDealt1 = hit.DamageDealt;
 			bool flag         = hit.Source.name.Contains("Great Slash") || hit.Source.name.Contains("Dash Slash") || hit.Source.name.Contains("Hit L") || hit.Source.name.Contains("Hit R");
+			bool furyAlwaysOn = ModHooks.GetMod("FuryAlwaysOn") is Mod mod;
 			if (hit.AttackType.ToString().Contains("Nail") & flag && PlayerData.instance.equippedCharm_25)
 			{
 				int damageDealt2 = hit.DamageDealt;
 				hit.DamageDealt = (int)(damageDealt2 * 1.5);
+				if (furyAlwaysOn && PlayerData.instance.equippedCharm_6)
+				{
+					int damageDealt3 = hit.DamageDealt;
+					hit.DamageDealt = (int)(damageDealt3 * 1.75);
+				}
+			}
+			else if (furyAlwaysOn && hit.AttackType.ToString().Contains("Nail") && PlayerData.instance.equippedCharm_6 && flag)
+			{
+				int damageDealt4 = hit.DamageDealt;
+				hit.DamageDealt = (int)(damageDealt4 * 1.75);
 			}
 			return hit;
 		}
